6148681ff089e6fd695d08a7d08a6b394980a086
Crow, Michael C <crowmc@ornl.gov>
2022-04-21 15:22:37 -0400

    reworked args - removed mutual exclusion from word-position args - removed positional letters arg - replaced conditionals with a single generator

--- word-search.py
+++ word-search.py
@@ -17,32 +17,32 @@ if not exists('words.txt'):
         exit(1)
 
 parser = ArgumentParser()
-parser.add_argument('letters', type=str, help='Beginning letters of the word')
+# TODO: Implement an alternative search with positional arg 'letters' and mutually exclusive optional [-b,-m,-e] args
+# parser.add_argument('letters', type=str, help='Beginning letters of the word')
 parser.add_argument('-l', '--length', type=int)
-
-group = parser.add_mutually_exclusive_group()
-group.add_argument('-m', '--mid', type=str)
-group.add_argument('-e', '--end', type=str)
-group.add_argument('-U', '--unscramble', action='store_true')
+parser.add_argument('-b', '-s', '--start',
+                          '--beg', type=str, default='', help='Beginning letters of the word', dest='beg')
+parser.add_argument('-m', '--mid', type=str, default='', help='Letters contained in the middle of the word, after any given beginning letters')
+parser.add_argument('-e', '--end', type=str, default='', help='Ending letters of the word')
+parser.add_argument('-U', '--unscramble', type=str, help='Invoke the Unscrambler on the given letters - can only be used independently or with the --length arg')
 args = parser.parse_args()
 
-if args.mid and args.end:
-    print('Cannot use --mid and --end together')
-    exit(1)
-elif args.mid and len(args.mid) < 2:
+if args.mid and len(args.mid) < 2:
     print('--mid must be more than one letter')
+    exit(1)
+if (args.beg or args.mid or args.end) and args.unscramble:
+    print('Cannot use positionals [beg, mid, end] with --unscramble argument')
     exit(1)
 
 with open('words.txt', 'r') as words:
     if args.unscramble:
         from unscrambler import Unscrambler
-        matches = Unscrambler(words).unscramble(args.letters)
-    elif args.end is None and args.mid is None:
-        matches = [ word.strip() for word in words if word.startswith(args.letters) ]
-    elif args.end:
-        matches = [ word.strip() for word in words if word.startswith(args.letters) and word.strip().endswith(args.end) ]
-    else: # args.mid
-        matches = [ word.strip() for word in words if word.startswith(args.letters) and args.mid in word.strip()[len(args.letters):] ]
+        matches = Unscrambler(words).unscramble(args.unscramble)
+    else:
+        matches = [word.strip() for word in words
+                   if word.strip().startswith(args.beg)
+                   and args.mid in word.strip()[len(args.beg):]
+                   and word.strip().endswith(args.end)]
 
 for word in matches:
     if args.length is None:
