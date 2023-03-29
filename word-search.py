ff5c6bc71a82c72a93c91054200eb08792afebfb
Crow, Michael C <crowmc@ornl.gov>
2023-03-29 14:35:07 -0400

    commented mid letter length requirement and added commented code for alternate exclusive args group if interested in exploring later

--- word-search.py
+++ word-search.py
@@ -17,6 +17,18 @@ if not exists('words.txt'):
 
 parser = ArgumentParser()
 parser.add_argument('-l', '--length', type=int, default=5, help='Only words with this length will be shown. [default:5]')
+
+# group = parser.add_argument_group('Flagged positionals')
+# group.add_argument('letters', type=str, help='Letters to search, default assumes beginning letters of the word', default='', nargs='?')
+# if not argv[1].startswith('-'):
+#     flags = group.add_mutually_exclusive_group(required=True)
+#     flags.add_argument('-b', '-s', '--start',
+#                              '--beg', action='store_true', help='Words beginning with', dest='beg')
+#     flags.add_argument('-m', '--mid', action='store_true', help='Words containing')
+#     flags.add_argument('-e', '--end', action='store_true', help='Words ending with')
+# else:
+# group = parser.add_mutually_exclusive_group(required=True)
+# positions = group.add_argument_group('Multiple position search')
 parser.add_argument('-b', '-s', '--start',
                           '--beg', type=str, default='', help='Beginning letters of the word', dest='beg')
 parser.add_argument('-m', '--mid', type=str, default='', help='Letters contained in the middle of the word, after any given beginning letters')
@@ -24,9 +36,9 @@ parser.add_argument('-e', '--end', type=str, default='', help='Ending letters of
 parser.add_argument('-U', '--unscramble', type=str, help='Invoke the Unscrambler on the given letters - can only be used independently or with the --length arg')
 args = parser.parse_args()
 
-if args.mid and len(args.mid) < 2:
-    print('--mid must be more than one letter')
-    exit(1)
+# if args.mid and len(args.mid) < 2:
+#     print('--mid must be more than one letter')
+#     exit(1)
 if (args.beg or args.mid or args.end) and args.unscramble:
     print('Cannot use positionals [beg, mid, end] with --unscramble argument')
     exit(1)
@@ -46,3 +58,4 @@ for word in matches:
         print(word)
     elif len(word) == args.length:
         print(word)
+# print(f'------\n{len(matches)}\n------')
